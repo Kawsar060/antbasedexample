c45562633573f1ee898f14cdfd41fd4a8da41b04
kawsar <kawsar.zaman1992@gmail.com>
2019-07-11 23:38:12 +0600

    another one

--- test/AppTest.java
+++ test/AppTest.java
@@ -1,11 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import InitialHello.MyJUnitClass;
 import junit.framework.*;
 
 public class AppTest extends TestCase {
     public void test1() {
+    	
+    	App junit = new App();
+		int result = junit.add(100, 200);
+		assertEquals(301, result);
     }
 
-    public void test2() {
-        // fail("test failed!");
-    }
+   
 }
 © 2019 GitHub, Inc.
